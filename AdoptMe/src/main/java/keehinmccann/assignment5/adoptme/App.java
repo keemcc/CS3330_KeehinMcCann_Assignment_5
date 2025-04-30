5a1fa2ec3f8a2ec223356388b46a3b89299de251
keemcc <ktsmccann@gmail.com>
2025-04-30 18:04:34 -0500

    initial commit

---
+++ AdoptMe/src/main/java/keehinmccann/assignment5/adoptme/App.java
@@ -0,0 +1,7 @@
+package keehinmccann.assignment5.adoptme;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ AdoptMe/src/test/java/keehinmccann/assignment5/adoptme/AppTest.java
@@ -0,0 +1,13 @@
+package keehinmccann.assignment5.adoptme;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
