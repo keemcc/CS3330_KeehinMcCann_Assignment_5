7bccf7566785b1298a29b707e5b51208213b3a2d
keemcc <ktsmccann@gmail.com>
2025-04-30 18:15:35 -0500

    Created Starts of Abstract Class Pet, Class ExoticPetAdapter,StandardPet
    Has Getters and setters created and fields

---
+++ AdoptMe/src/main/java/keehinmccann/assignment5/adoptme/model/pet/ExoticPetAdapter.java
@@ -0,0 +1,9 @@
+package keehinmccann.assignment5.adoptme.model.pet;
+
+public class ExoticPetAdapter extends Pet {
+
+	public ExoticPetAdapter(int id, String name, String type, String species, int age, boolean adopted) {
+		super(id, name, type, species, age, adopted);
+	}
+
+}
---
+++ AdoptMe/src/main/java/keehinmccann/assignment5/adoptme/model/pet/StandardPet.java
@@ -0,0 +1,9 @@
+package keehinmccann.assignment5.adoptme.model.pet;
+
+public class StandardPet extends Pet {
+
+	public StandardPet(int id, String name, String type, String species, int age, boolean adopted) {
+		super(id, name, type, species, age, adopted);
+	}
+
+}
