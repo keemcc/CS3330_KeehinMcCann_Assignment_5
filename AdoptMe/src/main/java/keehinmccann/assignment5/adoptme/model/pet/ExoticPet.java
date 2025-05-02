9754e89a392f6369e725b3ee83e56ed5dd9034e4
keemcc <ktsmccann@gmail.com>
2025-05-01 19:02:55 -0500

    Created ExoticPet

---
+++ AdoptMe/src/main/java/keehinmccann/assignment5/adoptme/model/pet/ExoticPet.java
@@ -0,0 +1,57 @@
+package keehinmccann.assignment5.adoptme.model.pet;
+
+public class ExoticPet {
+	private String uniqueId;
+	private String animalName;
+	private String category;
+	private String subSpecies;
+	public String getUniqueId() {
+		return uniqueId;
+	}
+
+	public void setUniqueId(String uniqueId) {
+		this.uniqueId = uniqueId;
+	}
+
+	public String getAnimalName() {
+		return animalName;
+	}
+
+	public void setAnimalName(String animalName) {
+		this.animalName = animalName;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getSubSpecies() {
+		return subSpecies;
+	}
+
+	public void setSubSpecies(String subSpecies) {
+		this.subSpecies = subSpecies;
+	}
+
+	public int getYearsOld() {
+		return yearsOld;
+	}
+
+	public void setYearsOld(int yearsOld) {
+		this.yearsOld = yearsOld;
+	}
+
+	private int yearsOld;
+	
+	public ExoticPet(String uniqueId, String animalName, String category, String subSpecies, int yearsOld) {
+		this.uniqueId = uniqueId;
+		this.animalName = animalName;
+		this.category = category;
+		this.subSpecies = subSpecies;
+		this.yearsOld = yearsOld;
+	}
+}
